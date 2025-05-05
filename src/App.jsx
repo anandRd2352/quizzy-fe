64a3be94fc0a9f6d92fb37f8fecb1f91f5c012f3
bbaravind2000 <bbaravind2000@gmail.com>
2025-05-05 19:40:31 +0530

    list of questions and instructions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Login from './assets/1.1.1'; // import your Login component
 import UserProfileUpdate from './assets/1.2.2';
+import AvailableExams from './assets/3.1.1';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
   return (
     <div>
-      <UserProfileUpdate />
+      <AvailableExams/>
     </div>
   );
 }
